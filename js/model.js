9bac23d9eda70ca0b7ba5065eaf6b9315cc41861
Deokti <deokneks@gmail.com>
2023-05-17 11:24:41 +0300

    Добавил событиия минимальной и максимальной стоимости

--- js/model.js
+++ js/model.js
@@ -2,6 +2,9 @@ import { Logger } from "./util/logger/Logger.js";
 
 let data = {
   selectedProgram: 0.1,
+  cost: 375000,
+  minPrice: 375000,
+  maxPrice: 100000000,
   programs: {
     base: 0.1,
     it: 0.047,
--- js/view/costInput.js
+++ js/view/costInput.js
@@ -1,13 +1,50 @@
 function init(getData) {
   const input = document.querySelector("#input-cost");
 
+  const { minPrice, maxPrice, cost } = getData();
+
   const settingCleave = {
     numeral: true,
     numeralThousandsGroupStyle: "thousand",
     delimiter: " ",
   };
 
-  new Cleave(input, settingCleave);
+  const cleaveInput = new Cleave(input, settingCleave);
+  cleaveInput.setRawValue(cost);
+
+  input.addEventListener("input", function () {
+    const value = parseInt(cleaveInput.getRawValue());
+    //Событие добавляющее ошибку при неверном минимальном и максимальном значении
+
+    if (value < minPrice || value > maxPrice) {
+      input.closest(".param__details").classList.add("param__details--error");
+    }
+
+    if (value >= minPrice && value <= maxPrice) {
+      input
+        .closest(".param__details")
+        .classList.remove("param__details--error");
+    }
+
+    input.addEventListener("change", function () {
+      const value = parseInt(cleaveInput.getRawValue());
+      //Событие при ошибке возвращает минимальное и максимальное значение
+
+      if (value < minPrice) {
+        input
+          .closest(".param__details")
+          .classList.remove("param__details--error");
+        cleaveInput.setRawValue(minPrice);
+      }
+
+      if (value > maxPrice) {
+        input
+          .closest(".param__details")
+          .classList.remove("param__details--error");
+        cleaveInput.setRawValue(maxPrice);
+      }
+    });
+  });
 }
 
 export default init;
